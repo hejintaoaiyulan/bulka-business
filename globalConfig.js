073966d2f59f65f3cd5edc8372207c97ea6ba043
hejt <15083594066@163.com>
2025-05-17 23:43:48 +0800

    fix: 修复bug2

--- globalConfig.js
+++ globalConfig.js
@@ -1,6 +1,7 @@
 export const Regs = {
-    mobile: /^\d+$/,
-    // mobile: /^1[3-9]\d{9}$/,
+	// 数字就行
+	mobile: /^\d+$/,
+	// mobile: /^1[3-9]\d{9}$/,
 }
 
 export const googleMapAppKey = 'AIzaSyC2dpdvm87rWSY3pocUfpxpkTawMHebSjw'
